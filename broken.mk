ead2a036cd19db9d70f2b48e4f6dfe51098bd86d
blkredstarV <blkredstar5@gmail.com>
2015-05-09 21:40:21 -0500

    Broke it

--- broken.mk
+++ broken.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
-# Inherit some common LiquidSmooth stuff.
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+# Inherit some common Broken stuff.
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2spr/d2spr:5.0.2/KOT49H/L710VPUDNJ2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2spr-user 5.0.2 KOT49H L710VPUDNJ2 release-keys"
 
-PRODUCT_NAME := liquid_d2spr
+PRODUCT_NAME := broken_d2spr
 PRODUCT_DEVICE := d2spr
 
